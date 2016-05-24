9481af52eee266546ff7960d9d4b1e2774c88e1d
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-05-24 00:28:25 -0300

    logging

--- base_study/event_loop_queue.js
+++ base_study/event_loop_queue.js
@@ -2,11 +2,13 @@ var http = require('http');
 
 var count = 1
 http.createServer(function (req, res) {
+  console.time('event');
   console.log('Request:' + count++);
   var num = 1;
   while(num < 5000000000){
     num++;
   }
+  console.timeEnd('event');
 	res.end("process end");
 }).listen(1337, '127.0.0.1');
 

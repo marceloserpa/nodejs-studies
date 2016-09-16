50e53ab5f6e9455c7d30d6b44c0363d6f135d4f7
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-09-16 20:55:49 -0300

    refactoring

--- fun-with-es6/server.js
+++ fun-with-es6/server.js
@@ -3,16 +3,19 @@ var reqpromise = require('request-promise');
 
 const options = {  
   method: 'GET',
-  uri: 'https://news.ycombinator.com'
+  uri: 'https://news.ycombinator.com',   
+  transform: function (body) {
+     return cheerio.load(body);
+  }
 }
 
-reqpromise(options).then((response) => {
-  var html = response;
-  $ = cheerio.load(html);
+reqpromise(options).then(($) => {
   $('.storylink').each(function(i, element){
       console.log($(this).text());
    });
 }).catch((err) =>  console.log(err));
+
+
 
 
 
